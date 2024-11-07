1d07f7d0ece275ec838fd3cc0b07a350379f25a4
Dory <pemegomachado@gmail.com>
2024-11-07 22:58:41 +0100

    Update 2
    Added /image command, that fetches a random danbooru image with the shin_megami_tensei tag :3

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import requests
+
 import generator
 
 intents = discord.Intents.default()
 
 bot = discord.Bot(intents=intents)
 
+load_dotenv()
 
 @bot.event
 async def on_ready() -> None:
@@ -33,5 +36,12 @@ async def create_joke(ctx: discord.ApplicationContext,
 async def answer(ctx: discord.ApplicationContext) -> None:
     await ctx.respond(generator.answer())
 
-load_dotenv()
+@bot.slash_command()
+async def image(ctx: discord.ApplicationContext) -> None:
+    r = requests.get(f'https://danbooru.donmai.us/posts/random?q=shin_megami_tensei',
+                     auth=(os.getenv("DANBOORU_USER"), os.getenv("DANBOORU_API_KEY")))
+    if r.status_code != 200:
+        await ctx.respond("Young man, my image dealer gave me this for you: RESPONSE CODE " + str(r.status_code))
+    await ctx.respond(f"Young man, [here]({r.url}) I have found a treasured picture of me and my friends. Please keep it safe.")
+
 bot.run(os.getenv("BOT_TOKEN"))
